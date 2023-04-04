1d2f3a9b6a0c248384b920c9b0f8e5122a07ade6
KlimentiyBritter <tesla8150@yandex.ru>
2023-04-05 01:20:24 +0300

    Task2.2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,19 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld secondBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage() + " " + secondBean.getMessage());
+
+        ApplicationContext applicationContextCat =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat beanCat =
+                (Cat) applicationContextCat.getBean("cat");
+        Cat secondBeanCat =
+                (Cat) applicationContextCat.getBean("cat");
+        System.out.println(beanCat.getString() + " " + secondBeanCat.getString());
+
+        System.out.println(bean == secondBean);
+        System.out.println(beanCat == secondBeanCat);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,10 @@
+public class Cat {
+    private String string;
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+}
